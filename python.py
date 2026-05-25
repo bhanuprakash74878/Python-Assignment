d1d7bb915c3a5a2a7dbea5afa1466da131a77698
bhanuprakash74878 <bhanuprakashputtam@gmail.com>
2026-05-25 11:54:08 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,19 @@
+#question number 1
+name = "Bhanu Prakash"
+Status = "Just completed intermediate"
+exams_written = "Jee Mains,Jee Advance,Ap Eapcet,VITEEE"
+rank = 17533
+Score = 91.322
+marks = 64
+apcet_marks = "Not known"
+Colleges = "Still not decided"
+results = 1,6,2026
+print(f"my name is {name}")
+print(f"I {Status}")
+print(f"I have written {exams_written}")
+print(f"I have scored {Score} in JEE Mains")
+print(f"Secured {rank} in VITEEE Exam")
+print(f"As of now,I got {marks} in JEE Advance exam")
+print(f"Still my eapcet marks were {apcet_marks}")
+print(f"Yet,{Colleges} the college that I wanted to join")
+print(f"It will be decided by {results}")
